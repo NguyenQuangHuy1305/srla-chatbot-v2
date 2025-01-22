158dffa87ce0caa824c7d8fc3bedbdfc2776ae21
Huy Nguyen <Mr831817toe@gmail.com>
2025-01-22 23:42:12 +1000

    fix 405 error #8

--- api/function_app.py
+++ api/function_app.py
@@ -7,7 +7,7 @@ import logging
 
 app = func.FunctionApp()
 
-@app.route(route="/api/chat", methods=['POST'])
+@app.route(route="chat")
 def chat(req: func.HttpRequest) -> func.HttpResponse:
     try:
         # Get request body
